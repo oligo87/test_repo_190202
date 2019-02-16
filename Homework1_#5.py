e92eaaa6738a56e642959d47d90084942cf58ac3
oligo87 <oligo@i.ua>
2019-02-16 11:01:01 +0200

    task 5

---
+++ Homework1_#5.py
@@ -0,0 +1,11 @@
+a = int(input('Input a: '))
+b = int(input('Input b: '))
+c = int(input('Input c: '))
+
+while not (a <= b <= c):
+    if a > b:
+	    a, b = b, a
+    elif b > c:
+	    b, c = c, b
+else:
+    print ('Sorted: {0}, {1}, {2}'.format(a, b, c))
