5920fe16c8898af0bd245ff9cb15d8eaef430b92
oligo87 <oligo@i.ua>
2019-03-27 23:23:31 +0200

    HW5 task3 upd

--- Homework 5/task3.py
+++ Homework 5/task3.py
@@ -1,12 +1,16 @@
+import string
+
 with open('source.txt') as source_file:
     text = source_file.read()
 
+text = text.casefold()
+text = text.translate(str.maketrans('', '', string.punctuation))
 list_words = text.split()
 
 sorted_list = list(set(list_words))
 sorted_list.sort()
 
-for i in sorted_list:
-    counter = list_words.count(i)
-    with open('sorted.txt', 'a') as new_file:
+with open('sorted.txt', 'a') as new_file:
+    for i in sorted_list:
+        counter = list_words.count(i)
         new_file.write(f'{i} {str(counter)}\n')
